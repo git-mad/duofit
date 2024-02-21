f91fc470078cbe6c01959de85eb7ec6fcfc4dcda
keane <wristy8@gmail.com>
2024-02-21 01:19:56 -0500

    feat: set up first registration page (copied)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,6 @@ All notable changes to this project will be documented in this file. See [standa
 
 ## 1.1.0 (2024-02-21)
 
-
 ### New features
 
-* added husky ([95129ac](https://github.com/git-mad/duofit/commit/95129aced9fde4fa50753e26deb3906b8fe6759b))
+- added husky ([95129ac](https://github.com/git-mad/duofit/commit/95129aced9fde4fa50753e26deb3906b8fe6759b))
---
+++ src/Register/Register.tsx
@@ -0,0 +1,35 @@
+import auth from '@react-native-firebase/auth'
+import React, { useState } from 'react'
+import { Button, TextInput, View } from 'react-native'
+
+export const Register = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const register = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('User account created & signed in!')
+      })
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!')
+        }
+
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!')
+        }
+
+        console.error(error)
+      })
+  }
+
+  return (
+    <View>
+      <TextInput placeholder="Email" onChangeText={(text) => setEmail(text)} />
+      <TextInput placeholder="Password" onChangeText={(text) => setPassword(text)} />
+      <Button title="Register" onPress={register} />
+    </View>
+  )
+}
