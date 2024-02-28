f7e5a44a3115e6e9d338bf6c8090ed5ca9e91d79
keane <wristy8@gmail.com>
2024-02-28 18:06:59 -0500

    fix: delete tests

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ const config = {
   setupFilesAfterEnv: [path.join(__dirname, 'setup-testing.js')],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
-  ]
+  ],
+  testPathIgnorePatterns: ['node_modules/*']
 }
 
 module.exports = config
