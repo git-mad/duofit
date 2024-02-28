2ff725b69ef8d55f7e32ed67bbb71b6abaa24c02
keane <wristy8@gmail.com>
2024-02-28 18:06:20 -0500

    feat: migrate from react native firebase to firebase.js

---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBTU1CrJgaEswM54synsoe3jl-jLAXHJPg',
+  authDomain: 'duofit-49c09.firebaseapp.com',
+  databaseURL: 'https://duofit-49c09-default-rtdb.firebaseio.com',
+  projectId: 'duofit-49c09',
+  storageBucket: 'duofit-49c09.appspot.com',
+  messagingSenderId: '372081593955',
+  appId: '1:372081593955:web:0cdab27436e19074f6f256',
+  measurementId: 'G-Z8VHB73J75'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+export { auth, app }
